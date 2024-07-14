c7565b9926944487ddf8946b3724decfbfbdf3f0
dron94 <andreypan1994@gmail.com>
2024-07-14 17:47:29 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+
+First_name = 12    #'Количество выполненых ДЗ'
+Tottal_time= 1.5   #'Количество затраченных часов'
+Second_name='Python'
+Time_for_one_task= (Tottal_time/First_name)  #Количество времени затраченное на одно задание
+print(Second_name,'Количество выполненых ДЗ:',First_name,'Количество затраченных часов:',Tottal_time,'Количество времени затраченное на одно задание:',Time_for_one_task)
